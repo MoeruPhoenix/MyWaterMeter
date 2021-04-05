09dedd2ef2dc7c0155f1d615abfadfb7a8b34782
Orrin Reid <59991691+MoeruPhoenix@users.noreply.github.com>
2021-04-05 01:02:41 -0500

    Add files via upload
    Users are now able to add and delete items in the reminders area of the dashboard

--- MyWaterMeter/main.py
+++ MyWaterMeter/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flaskext.mysql import MySQL
 from datetime import datetime
 from decimal import Decimal
@@ -6,6 +6,7 @@ from decimal import Decimal
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 mysql = MySQL()
 mysql.init_app(app)
  
@@ -26,6 +27,7 @@ def index():
     reading = str(reading)
     reading = re.sub("\D", "", reading)
     #reading = Decimal(reading)
+    
     cur.close()
     
     curpH = mysql.get_db().cursor()
@@ -35,6 +37,14 @@ def index():
     phreading = phreading[3] + phreading[4]
     phreading = re.sub("\D", "", phreading)
     curpH.close()
+    
+    
+    curRem = mysql.get_db().cursor()
+    res = curRem.execute ("SELECT * FROM reminderTbl")
+    rem = curRem.fetchall()
+    curRem.close()
+    
+    print(rem)
      
     curTs = mysql.get_db().cursor()
     curTs.execute("SELECT timestamp FROM waterLevel ORDER BY rID DESC LIMIT 1")
@@ -42,16 +52,16 @@ def index():
     
     now = datetime.now()
     time = now.strftime("%H:%M:%S") 
-    curTs.close()
+    curTs.close() 
     print (ts)
-    tsa = ts[0]
+    tsa = ts[0] 
     
     #tsa = tsa.strftime('%Y-%m-%d::%H-%M')
     print(tsa)
     
     print(reading)
         
-    return render_template('index.html', result=reading, pHresult=phreading, timeresult=time)
+    return render_template('index.html', result=reading, pHresult=phreading, timeresult=time, amm=rem)
 
 @app.route('/about') 
 def about():
@@ -69,5 +79,31 @@ def faq():
 def sitemap():
     return render_template('SiteMap.html')
 
+
+@app.route('/addReminder', methods=['GET', 'POST'])
+def addReminder():
+    if request.method == 'POST':
+        Water_level= int(request.form['wlevel'])
+        descrip = request.form['descrp']
+        
+        connection = mysql.get_db()
+        curRem = mysql.get_db().cursor()
+        curRem.execute("INSERT INTO reminderTbl (Description, WaterLevel) VALUES (%s, %s)", (descrip, Water_level))
+        connection.commit()
+        flash("Reminder Successfully Added!")
+        curRem.close()
+        return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>', methods=['GET'])
+def delete(id):
+    connection = mysql.get_db()
+    curD = mysql.get_db().cursor()
+    curD.execute("DELETE FROM reminderTbl WHERE SN=%s", (id,))
+    connection.commit()
+    curD.close()
+    flash("Reminder Successfully Deleted!")
+    
+    return redirect(url_for('index'))
+    
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0') 
